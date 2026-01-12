86b31fbe7ab1d361115937bb96cc8bc70fffd30c
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2026-01-12 22:13:58 +0530

    Server Created

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-i;
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+//app configs
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middlewares
+
+app.use(express.json());
+app.use(cors());
+
+//api endpoint
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => console.log("server Started", port));
