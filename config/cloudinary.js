011729f40655678d4e32fc8515ed68a937e26ad0
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2026-01-15 01:30:30 +0530

    Created API to add doctor data in database

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,15 @@
 // config/cloudinary.js
 import { v2 as cloudinary } from "cloudinary";
+// import { cloudinary } from "../config/cloudinary.js";
 
-const connectCloudinary = async () => {
+const connectCloudinary = () => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
+
+  console.log("✅ Cloudinary configured");
 };
 
-export default cloudinary;
+export { cloudinary, connectCloudinary };
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,89 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+
+import doctorModel from "../models/doctorModel.js";
+import { json } from "express";
+
 // API for adding doctor
-const addDoctor = async (req,res) => {
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
 
-    try {
+    //checking for all data to add doctor
+    //check for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !fees ||
+      !degree ||
+      !address
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing details" });
+    }
+    // validating email format
 
-        const  = { name, email, password, speciality, degree, experience, about, fees, address }
-        const imageFile = req.file
-
-    } catch (error) {
-
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter a valid email" });
     }
 
-}
+    // validating strong password
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter strong password ",
+      });
+    }
 
-export {addDoctor}
+    //hashing doctor password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //upload image to cloudnary
+
+    //upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorSchema = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+    const newDoctor = new doctorModel(doctorSchema);
+    await newDoctor.save();
+    return res.json({ success: true, message: "Doctor Added " });
+  } catch (error) {
+    console.log("error", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor };
