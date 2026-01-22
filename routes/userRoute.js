cc3afaaf064674c29e3f881330cad9bff0bbed0d
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2026-01-22 09:07:28 +0530

    API to create appointment booking functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   registerUser,
   updateProfile,
+  bookAppointment,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 import upload from "../middlewares/multer.js";
@@ -19,5 +20,6 @@ userRouter.post(
   upload.single("image"),
   updateProfile,
 );
+userRouter.post("/book-appointment", authUser, bookAppointment);
 
 export default userRouter;
